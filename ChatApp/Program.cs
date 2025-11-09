adae05c17d1d74b0149abd368f7d8ec88fbec168
oleh <115097045+olehone@users.noreply.github.com>
2025-11-09 11:06:40 +0200

    Use dedicated SignalR service

--- ChatApp/Program.cs
+++ ChatApp/Program.cs
@@ -2,6 +2,7 @@ using ChatApp.Models;
 using ChatApp.Data;
 using ChatApp.Hubs;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Azure.SignalR;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,9 +12,11 @@ builder.Services.AddLogging(config =>
     config.AddConsole();
     config.AddDebug();
 });
-builder.Services.AddSignalR();
 
-var connectionString = Environment.GetEnvironmentVariable("SQLAZURECONNSTR_AZURE_SQL_CONNECTIONSTRING")
+builder.Services.AddSignalR().AddAzureSignalR();
+
+var connectionString =
+    Environment.GetEnvironmentVariable("SQLAZURECONNSTR_AZURE_SQL_CONNECTIONSTRING")
     ?? throw new Exception("Database connection string not found.");
 
 builder.Services.AddDbContext<ChatDbContext>(options =>
@@ -24,13 +27,20 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-
 app.UseSwagger();
 app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
-app.UseAuthorization();
-app.MapControllers();
-app.MapHub<ChatHub>("/chathub");
+
 app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+    endpoints.MapHub<ChatHub>("/chathub");
+});
+
 app.Run();
